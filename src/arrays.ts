cb6f36817490bf7571628822b35d76ab4990051e
Blarnix <61402697+Blarnix@users.noreply.github.com>
2025-03-13 23:15:03 -0400

    Wrote bookEndList function.

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,6 +5,15 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
+    if (numbers.length === 0) {
+        return [];
+    }
+    if (numbers.length === 1) {
+        return [numbers[0], numbers[0]];
+    }
+    if (numbers.length > 1) {
+        return [numbers[0], numbers[numbers.length - 1]];
+    }
     return numbers;
 }
 
